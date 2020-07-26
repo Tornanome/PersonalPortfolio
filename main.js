5cd0a544508de9b215394b5db5b5d37678c519a3
Derek Balkman <tornanom1234@gmail.com>
2020-07-25 19:33:50 -0600

    Commited my first edit to main.js with setting up the javascript

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
 import { films } from './films.js';
-import { films } from './people.js';
+import { people } from './people.js';
+
+console.log(films)
+
+films.map(films => {
+    console.log(film.title)
+})
+
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el){
+    return parent.appendChild(el);
+}
+
+const swurl = films;
+
+fetch(swurl)
+    .then(resp => resp.jason())
+    .then(data => {
+        console.log(data)
+    })
