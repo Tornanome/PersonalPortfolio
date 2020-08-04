3de28ab684744e8fffeaf1773fbec368f44e6595
Derek Balkman <tornanom1234@gmail.com>
2020-08-04 16:25:08 -0600

    started editing my code about Star Wars and making my information look like cards for the the baseball cards assignment. I editing the index.html, style.css, and main.js files

--- main.js
+++ main.js
@@ -15,6 +15,41 @@ function append(parent, el){
 const url = 'https://swapi.dev/api/';
 const ul = document.getElementById('starWars');
 
+const getPeople = () => {
+    fetch('http://swapi.dev/api/people/')
+        .then(resp => resp.json())
+        .then(data => {
+
+            const people = data.results;
+
+            const redefinePerson = (person, idx) => {
+                return {
+                    name: person.name,
+                    dob: person.birth_year,
+                    gender: person.gender,
+                    height: person.height,
+                    id: idx
+                }
+            }
+        
+            const mySWpeopleArray = people.map(redefinePerson)
+
+            console.log(mySWpeopleArray)
+
+            const peopleHTML = mySWpeopleArray.map(person => {
+                return
+                <div class="scene">
+                    <div class="card">
+                        <div class="card_face card_face--front">${person.name}</div>
+                        <div class="card_face card_face--back"><div><img src='/swImg/${person.id}.jpg'/></div></div>
+                    </div>
+                </div>
+            })
+        })
+}
+
+getPeople();
+
 fetch(url)
     .then(resp => resp.json())
     .then(data => {
