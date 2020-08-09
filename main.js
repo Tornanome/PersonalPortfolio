20077135d58319de114c415464ea9b3404137a1f
Derek Balkman <tornanom1234@gmail.com>
2020-08-08 23:30:43 -0600

    added a new css file. added a area for the images spent 7 hours tring to figure out how to get it to display 25 people. Edited a lot on the main.js file. Also edited the swpage.html to display the baseball cards.

--- main.js
+++ main.js
@@ -1,15 +1,4 @@
-import { films } from './films.js';
-import { people } from './people.js';
-
-var cards = document. querySelectorAll('.card');
-
-cards.forEach(card => {
-    card.addEventListener( 'click', function() {
-        card.classList.toggle('is-flipped');
-    });
-})
-
-const container = document.querySelector('#container')
+const container = document.querySelector('#container');
 
 const getPeople = () => {
     fetch('http://swapi.dev/api/people/')
@@ -28,41 +17,61 @@ const getPeople = () => {
                 }
             }
         
-            const mySWpeopleArray = people.map(redefinePerson)
+            const mySWpeopleArray = people.map(redfinePerson)
 
             console.log(mySWpeopleArray)
 
             const peopleHTML = mySWpeopleArray.map(person => {
-                return `
-                <div class="scene">
-                    <div class="card">
-                    <div class="card_face card_face--front">${person.name}</div>
-                    <div class="card_face card_face--back"><div><img src='/swImg/${person.id}.jpg'/></div></div>
+                    return `
+                    <div class="scene">
+                        <div class="card">
+                        <div class="card_face card_face--front">${person.name}</div>
+                        <div class="card_face card_face--back"><div><p><img src='img/swImg/characters/${person.id}.jpg'/></p></div></div>
+                        </div>
                     </div>
-                </div>
-                `
+                    `
             }).join('');
+
+            console.log(peopleHTML)
            
             container.innerHTML = peopleHTML;
+            
+            var cards = document.querySelectorAll('.card');
+
+            cards.forEach(card => {
+                card.addEventListener( 'click', function() {
+                    card.classList.toggle('is-flipped');
+                });
+            })
+
         })
 }
 
 getPeople();
 
-fetch(url)
-    .then(resp => resp.json())
-    .then(data => {
-        film = data.results;
-        console.log(films)
+const charForm = document.querySelector('')
 
-        films.map(films => {
-            console.log(films.cell)
-            let li = createNode('li'),
-                span = createNode('span');
-
-            span.innerHTML = `${films.episode_id}`;
-            
-            append(li, span)
-        })
-       
-    })
+function get30SWPeople(params) {
+  let people = [];
+  const SWPromises = [
+      fetch('https://swapi.dev/api/people/?page=1'),
+      fetch('https://swapi.dev/api/people/?page=2'),
+      fetch('https://swapi.dev/api/people/?page=3')
+  ]
+ return Promise.all(SWPromises)
+  .then(ResponsesArr => {
+      return Promise.all(
+          ResponsesArr.map(data => data.json())
+          ) 
+  })
+  .then(jsonDataArr => {
+      people = jsonDataArr.reduce(
+          (acc, data) => [...acc, ...data.results]
+          , people)
+      return people;
+  })
+}
+get30SWPeople().then(people => {
+  console.log(people)
+  
+});
