e8d2fe84dc1c4f733995cac5f90c4ac0d58b7a44
Derek Balkman <tornanom1234@gmail.com>
2020-08-13 15:53:52 -0600

    added the pokemon to the pokedex page and styled them

--- main.js
+++ main.js
@@ -44,3 +44,43 @@ const getPeople = () => {
 }
 
 getPeople();
+
+/* Pokedex javascript */
+
+const pokedex = document.getElementById('pokedex');
+
+const fetchPokemon = () => {
+    const promises = [];
+    for (let i = 1; i <= 721; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then((res) => res.json()));
+    }
+    Promise.all(promises).then((results) => {
+        const pokemon = results.map((result) => ({
+            name: result.name,
+            image: result.sprites['front_default'],
+            type: result.types.map((type) => type.type.name).join(', '),
+            id: result.id
+        }));
+        displayPokemon(pokemon);
+    });
+};
+
+const displayPokemon = (pokemon) => {
+    console.log(pokemon);
+    const pokemonHTMLString = pokemon
+        .map(
+            (pokeman) => `
+        <li class="pokecard">
+            <img class="pokecard-image" src="${pokeman.image}"/>
+            <h2 class="pokecard-title">${pokeman.id}. ${pokeman.name}</h2>
+            <p class="pokecard-subtitle">Type: ${pokeman.type}</p>
+        </li>
+    `
+        )
+        .join('');
+    pokedex.innerHTML = pokemonHTMLString;
+};
+
+fetchPokemon();
+
