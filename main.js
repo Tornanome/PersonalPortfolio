f7c4d7559ec9b4c93d3ecdcc4c77bc45b59e44e9
Derek Balkman <tornanom1234@gmail.com>
2020-08-15 09:02:57 -0600

    tried to fix my star wars page on netlify

--- main.js
+++ main.js
@@ -44,34 +44,3 @@ const getPeople = () => {
 }
 
 getPeople();
-
-const personForm = document.querySelector('#personForm');
-
-personForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-
-    const formData = new FormData(personForm);
-
-    let personObj = new Object();
-
-    for(let pair of formData.entries()) {
-        console.log(pair)
-
-        personObj[pair[0]] = pair[1]
-    }
-
-    newPersonHTML = `
-    <div class="scene">
-        <div class="card">
-            <div class="card__face card__face--front">${person.name}</div>
-            <div class="card__face card__face--back"><div><img src='img/swImg/characters/${person.id}.jpg'/></div></div>
-        </div>
-    </div>
-    `
-
-    const parentDiv = document.createElement('div');
-    parentDiv.className('scene')
-
-    container.inenerHTML = newPersonHTML;
-
-})
